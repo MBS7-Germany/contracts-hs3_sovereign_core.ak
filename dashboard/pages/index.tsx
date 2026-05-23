c0d04b36bd05f33347ab396a4de9af2fec070598
MBS7-Germany <MBS017@web.de>
2026-05-23 23:31:29 +0200

    Add Home component with HS3CommandCenter

---
+++ dashboard/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import HS3CommandCenter from '../HS3Command';
+
+export default function Home() {
+  return (
+    <div className="bg-[#020712] min-h-screen">
+      <HS3CommandCenter />
+    </div>
+  );
+}
